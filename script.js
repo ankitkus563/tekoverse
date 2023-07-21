0f0a1ee803e93e06d33ed597072924d9eb4fb403
Ankit <ankitkumar563563@gmail.com>
2023-07-22 01:59:22 +0530

    added new card on website which have functionality to change img and color on hover

---
+++ script.js
@@ -0,0 +1,18 @@
+let cardInsider = document.getElementsByClassName("card-insider");
+let Cards = document.getElementsByClassName("Cards");
+
+for(let i=0; i<cardInsider.length; i++){
+
+    cardInsider[i].addEventListener("pointerenter", () => {
+
+        cardInsider[i].classList.add("card-insider2");
+        Cards[i].classList.add("Cardgreen");
+    });
+    cardInsider[i].addEventListener("pointerleave", () => {
+
+        cardInsider[i].classList.remove("card-insider2");
+        Cards[i].classList.remove("Cardgreen");
+    });
+}
+
+console.log(Cards);
